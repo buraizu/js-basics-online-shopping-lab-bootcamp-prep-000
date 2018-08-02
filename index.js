d4738a0d987ec66217dae1071f0e0d7241770d43
whoozit.whatsit@gmail.com <whoozit.whatsit@gmail.com>
2018-08-02 08:22:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,8 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`;
   } else if(cardNumber) {
+      cart = [];
       return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-    cart = [];
+    
   }
 }
