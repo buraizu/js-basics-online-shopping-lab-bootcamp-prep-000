44cb18dd6b1377311cc95b90883aa49f2fff3d8f
whoozit.whatsit@gmail.com <whoozit.whatsit@gmail.com>
2018-08-02 08:03:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,8 +51,7 @@ function removeFromCart(item) {
       cart.splice(i, 1);
       return cart;
     } 
-  }
-  'That item is not in your cart.'
+  } return `That item is not in your cart.`;
   return cart;
 }
 
