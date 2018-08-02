fc373fbb6be07e421f53cb3afeb8f802706e2bbd
whoozit.whatsit@gmail.com <whoozit.whatsit@gmail.com>
2018-08-02 03:26:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,14 @@ function addToCart(item) {
 function viewCart() {
   let displayCart = [];
   for (let i = 0; i < cart.length; i++) {
-    displayCart.push[i];
+    displayCart.push(cart[i]);
+    }
+    if (displayCart.length === 0){
+      return `Your shopping cart is empty.`;  
+    } else if (displayCart.length === 1) {
+      return `In your cart, you have ${displayCart.itemName[0]} at ${displayCart.itemPrice[0]}.`
+    } else if (displayCart.length === 2) {
+      return `In your cart, you have ${displayCart}`
     }
   }
 
